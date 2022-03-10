31c01e32a75f2cccce42f98677d52d79247c2f3d
SakhrErr <sakhr.hamza.1@gmail.com>
2022-03-10 16:25:55 +0100

    First iteration

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,9 @@
+public class FizzBuzz {
+    public static String printList(int x) {
+        return String.format(
+                "1 => 1\n" +
+                "2 => 2\n" +
+                "3 => Fizz\n"
+        );
+    }
+}
